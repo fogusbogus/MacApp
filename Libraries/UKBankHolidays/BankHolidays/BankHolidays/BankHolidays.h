368167567cdf0f1c60710020d5b4a408f0c39288
Matt Hogg <matt.hogg74@hotmail.com>
2020-09-14 00:20:55 +0100

    Post-creation

---
+++ Libraries/UKBankHolidays/BankHolidays/BankHolidays/BankHolidays.h
@@ -0,0 +1,19 @@
+//
+//  BankHolidays.h
+//  BankHolidays
+//
+//  Created by Matt Hogg on 13/09/2020.
+//  Copyright © 2020 Matt Hogg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BankHolidays.
+FOUNDATION_EXPORT double BankHolidaysVersionNumber;
+
+//! Project version string for BankHolidays.
+FOUNDATION_EXPORT const unsigned char BankHolidaysVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankHolidays/PublicHeader.h>
+
+
