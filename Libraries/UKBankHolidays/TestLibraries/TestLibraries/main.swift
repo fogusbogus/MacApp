5839bbcccbea0fd1b041c552cb646f937287d9d9
Matt Hogg <matt.hogg74@hotmail.com>
2020-09-13 22:20:34 +0100

    packages.swift

---
+++ Libraries/UKBankHolidays/TestLibraries/TestLibraries/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TestLibraries
+//
+//  Created by Matt Hogg on 13/09/2020.
+//  Copyright © 2020 Matt Hogg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
