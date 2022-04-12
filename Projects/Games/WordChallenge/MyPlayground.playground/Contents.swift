6a32213d6fbf36117c9acd3607ae882cb3ace2f7
Matt Hogg <matt.hogg74@hotmail.com>
2022-04-12 14:23:20 +0100

    Trying Xcode Server

--- Projects/Games/WordChallenge/MyPlayground.playground/Contents.swift
+++ Projects/Games/WordChallenge/MyPlayground.playground/Contents.swift
@@ -1,7 +1,12 @@
 import Cocoa
 
-func someFunc() -> (x: Int, y: Int) {
-	return (4,5)
-}
+var a = [121, 144, 19, 161, 19, 144, 19, 11]
+var b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
 
-print(someFunc())
+let c = a.filter { av in
+	return b.first { bv in
+		return bv % av == 0
+	} != nil
+}.count == a.count
+
+print(c)
