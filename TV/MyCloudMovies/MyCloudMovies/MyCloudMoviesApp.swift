f477560409a2eaaf8c540f3c6b94d3c0e790f41d
Matt Hogg <matt.hogg74@hotmail.com>
2022-11-04 10:26:25 +0000

    Rotation, delectability. Title and subject still not quite storing/retrieving correctly.

---
+++ TV/MyCloudMovies/MyCloudMovies/MyCloudMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCloudMoviesApp.swift
+//  MyCloudMovies
+//
+//  Created by Matt Hogg on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCloudMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
