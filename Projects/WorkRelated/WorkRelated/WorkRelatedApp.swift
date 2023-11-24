eca153ea074c20feda057cf59c02bec2bc33ce51
Matt Hogg <matt.hogg74@hotmail.com>
2023-11-24 15:36:22 +0000

    Slide event, fix of translation of timecode to delta

---
+++ Projects/WorkRelated/WorkRelated/WorkRelatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkRelatedApp.swift
+//  WorkRelated
+//
+//  Created by Matt Hogg on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkRelatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
