1059f75bb2ceca7f1e1b29f594d6449c3a096d69
Matt Hogg <matt.hogg74@hotmail.com>
2023-11-28 01:33:06 +0000

    2/3rds of Emphasis complete.

---
+++ Projects/Curves/Curves/CurvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurvesApp.swift
+//  Curves
+//
+//  Created by Matt Hogg on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
